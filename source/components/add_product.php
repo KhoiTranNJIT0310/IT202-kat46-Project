2da6ac130390a005c0bd309b01afddcfceacc742
KhoiTran0310 <kat46@njit.edu>
2024-03-21 14:41:34 -0400

    3/21

--- source/components/add_product.php
+++ source/components/add_product.php
@@ -1,11 +1,17 @@
-<?php 
+<!-- Khoi Tran 
+March 10  16th 2024
+IT 202-002
+Phase 3 Assignment:  Read SQL Data using PHP
+kat46@njit.edu -->
+<?php
 $sustaincategories_id = filter_input(INPUT_POST, 'sustaincategories_id', FILTER_VALIDATE_INT);
-print_r($_POST);
-$code = filter_input(INPUT_POST, 'code');
+// print_r($_POST);
+$code = filter_input(INPUT_POST, 'code' );
 $name = filter_input(INPUT_POST, 'name');
 $description = filter_input(INPUT_POST, 'description');
-$price = filter_input(INPUT_POST, 'price');
+$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
 
+// fetch the code in the database to check if the code is already used
 require_once('database_njit.php');
 $query = 'SELECT * FROM sustainitems WHERE sustainCode = :code';
 $statement = $db->prepare($query);
@@ -15,45 +21,76 @@ $exist_code = $statement->fetch();
 $statement->closeCursor();
 
 // Validate inputs
-$max_price =30;
+$max_price = 30;
 $error = "";
-if ($sustaincategories_id == NULL || $sustaincategories_id == FALSE){
+// check for the file get the data
+if ($sustaincategories_id == NULL || $sustaincategories_id == FALSE) {
     $error = "We couldn't get the data!";
 }
-else if ($code == NULL){
+// check if the code is empty
+else if ($code == NULL) {
     $error = " Product code cannot be empty!";
 }
-else if ($description == NULL){
+// check if the description is empty
+else if ($description == NULL) {
     $error = " Product description cannot be empty!";
 }
-else if ($name == NULL){
+// check if the name is empty
+else if ($name == NULL) {
     $error = " Product name cannot be empty!";
 }
-else if ($price == NULL || $price == FALSE || is_int($price) || $price <= 0 ){
+// check if the price is empty or not a float or invalid
+else if ($price == NULL || $price == FALSE || is_int($price) || $price <= 0) {
     $error = " Product price cannot be empty nor be an integer!";
 }
-else if ($price >$max_price){
+// check if the price is less than max value
+else if ($price > $max_price) {
     $error = " Product price exceed 30$ ";
 }
-else if ($exist_code ){
+// check if the product code is used 
+else if ($exist_code) {
     $error = "Product code has already been used!";
 }
+// if all are valid we display success 
 
+else {
+    require_once('database_njit.php');
+    // Add the product to the database  
+    $query = 'INSERT INTO  sustainitems (sustainCategoryID, sustainCode, sustainName, description, price, dateCreated) VALUES (:sustaincategories_id, :code,:name, :description, :price, NOW())';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':sustaincategories_id', $sustaincategories_id);
+    $statement->bindValue(':code', $code);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':price', $price);
+    $success = $statement->execute(); // execute
+    $statement->closeCursor();
+    if ($success == 1) {
+        $success = "good!";
+    } else {
+        $success = " not good!";
+    };
+    echo "<p>Your insert statement status is $success </p>";
+}
+// Show error if there is any
+?>
+<html>
 
-        else{
-            require_once('database_njit.php');
-            // Add the product to the database  
-            $query = 'INSERT INTO  sustainitems (sustainCategoryID, sustainCode, sustainName, description, price, dateCreated) VALUES (:sustaincategories_id, :code,:name, :description, :price, NOW())';
-            $statement = $db->prepare($query);
-            $statement->bindValue(':sustaincategories_id', $sustaincategories_id);
-            $statement->bindValue(':code', $code);
-            $statement->bindValue(':name', $name);
-            $statement->bindValue(':description', $description);
-            $statement->bindValue(':price', $price);
-            $success = $statement->execute();
-            $statement->closeCursor();
-            echo "<p>Your insert statement status is $success </p>";
-        }
-        echo "$error <br>"; 
-        ?>
-        <p><a href="product.php">View Product List</a></p>
+<head>
+    <title>Create Product</title>
+    <link rel="icon" href="../images/logo.png" type="image/png">
+    <link rel="stylesheet" href="../style/create.css">
+</head>
+
+<body>
+    <?php
+    include("header.php");
+    // Show error if there is any
+    ?>
+    <div class="container">
+    <h1><?php echo "$error <br>"; ?> </h1>
+    <button class="button"><a href="product.php">View Product List</a></button>
+    </div>
+</body>
+
+</html>
--- source/components/add_product_form.php
+++ source/components/add_product_form.php
@@ -4,10 +4,11 @@ IT 202-002
 Phase 3 Assignment:  Read SQL Data using PHP
 kat46@njit.edu -->
 <?php
+// get the database
 require_once('database_njit.php');
 $query = 'SELECT * FROM sustaincategories ORDER BY sustainCategoryID';
 $statement = $db->prepare($query);
-$statement->execute();
+$statement->execute(); //execute;
 $sustaincategories = $statement->fetchAll();
 $statement->closeCursor();
 ?>
@@ -26,32 +27,34 @@ $statement->closeCursor();
 include("header.php");
 ?>
     <header class="container">
-        <h1  class="container" > Product Manager</h1>
+        <h1> Product Manager</h1>
         </header>
         <main>
             <form action="add_product.php" method="POST" class="create_product_form">
                 
             <h1> Add Product</h1>
                 <label for="">Category:</label>
+                <!-- display the list of changing options  -->
                 <select  class="option" name="sustaincategories_id">
                     <?php foreach ($sustaincategories as $sustaincategory) : ?>
-                        <option  class="option" value="<?php echo $sustaincategory['sustainCategoryID']; ?>"><?php echo $sustaincategory['sustainCategoryName'] ?></option>
+                        <option value="<?php echo $sustaincategory['sustainCategoryID']; ?>"><?php echo $sustaincategory['sustainCategoryName'] ?></option>
                     <?php endforeach; ?>
 
                 </select>
                 <br>
+                <!-- Ask for the product code -->
                 <label>Code:</label>
                 <input type="text" name="code"><br>
-
+                <!-- Ask for the product name -->
                 <label>Name:</label>
                 <input type="text" name="name"><br>
+                <!-- Ask for the product description -->
                 <label>Description:</label>
-
                 <input type="text" name="description"><br>
-
+                <!-- Ask for the product price -->
                 <label>List Price:</label>
                 <input type="text" name="price" placeholder="Price must less than 30.00$"><br>
-
+                <!-- submit button -->
                 <input class = "button" type="submit" value="Add Product"><br>
                 <!-- <p ><a href="product.php">View Product List</a></p> -->
             </form>
